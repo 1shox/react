ef975b546f3b46cbdcedb8fd383dd9787788ad41
1Shox <shaxriyorqobiljonov18@gmail.com>
2024-11-02 13:35:35 +0000

    dfcgvhbjn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-
+import React from 'react';
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';    
 function App() {
   return (
     <div className="App">
-
+        <div>
+        <Navbar />
+        </div>
+        <div className='main'>
+          Main
+        </div>
+        <div>
+        <Footer />
+        </div>
     </div>
   );
 }
